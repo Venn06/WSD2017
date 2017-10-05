a6ccc612689d79814d67395b28830b1f0604ea9b
Prestidgeous <31885339+Prestidgeous@users.noreply.github.com>
2017-10-05 19:43:12 +1100

    -a

---
+++ UTStutor/src/java/uts/wsd/soap/UTStutorservice.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.wsd.soap;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author rhysp
+ */
